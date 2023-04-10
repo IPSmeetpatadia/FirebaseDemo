ffbefb0744c7a9de2c94d0679f5a4e7672cf7bcb
IPS-030 <meetp-itpath@gmail.com>
2023-04-10 12:56:36 +0530

    DONE: PDF Upload

--- app/src/main/java/com/ipsMeet/firebasedemo/fragment/DocumentListFragment.kt
+++ app/src/main/java/com/ipsMeet/firebasedemo/fragment/DocumentListFragment.kt
@@ -32,7 +32,6 @@ class DocumentListFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
